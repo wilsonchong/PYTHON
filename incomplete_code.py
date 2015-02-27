d56c7bdfca5e9444c28292b63f7c73f76a1b1d13
tintinkmitl <yougtin@gmail.com>
2015-02-27 15:23:36 +0700

    edited finish!

--- incomplete_code.py
+++ incomplete_code.py
@@ -21,17 +21,30 @@ class Walk( Transportation ):
    def find_cost( self ):
       return 0
 
+class Taxi( Transportation ):
+   def __init__( self, start, end, distance):
+      Transportation.__init__( self, start, end, distance)
+      self.distance = distance
+   def find_cost( self ):
+      return 40 * self.distance
+   
+class Train( Transportation ):
+   def __init__( self, start, end, distance, numberofstation):
+      Transportation.__init__( self, start, end, distance)
+      self.num = numberofstation
+   def find_cost( self ):
+      return 5 * self.num
 
    
 # main program
 
 travel_cost = 0
 
-#trip = [ Walk("KMITL","KMITL SCB Bank",0.6),
-         #Taxi("KMITL SCB Bank","Ladkrabang Station",5),
-         #Train("Ladkrabang Station","Payathai Station",40,6),
-         #Taxi("Payathai Station","The British Council",3) ]
+trip = [ Walk("KMITL","KMITL SCB Bank",0.6),
+         Taxi("KMITL SCB Bank","Ladkrabang Station",5),
+         Train("Ladkrabang Station","Payathai Station",40,6),
+         Taxi("Payathai Station","The British Council",3) ]
 
 for travel in trip:
    travel_cost += travel.find_cost()
-print travel_cost
+print (travel_cost)
